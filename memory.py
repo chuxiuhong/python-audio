293a4f7b1e75a293070531bf459a4fcfb3da68c0
chuxiuhong <nanwugongzi@vip.qq.com>
2016-09-28 13:28:38 +0800

    增加了数据库管理，但是有bug

--- memory.py
+++ memory.py
@@ -1,9 +1,17 @@
-#coding=utf-8
+# coding=utf-8
+
+import os
 
 import MySQLdb
 
+
 class memory():
-    def AddNewSong(self,path,):
+    def AddNewSong(self, path):
+        basename = os.path.basename(path)
         conn = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='', db='fingerprint')
         cur = conn.cursor()
-        cur.execute()
+        cur.execute("insert into song VALUES(NULL," + basename + ');')
+
+
+sss = memory()
+sss.AddNewSong('C:\data\music\\audio\\audio\\ (1).wav')
