5b758166545852458d660bcea7fcc63c64d3377d
chuxiuhong <nanwugongzi@vip.qq.com>
2016-10-08 15:57:13 +0800

    莫名刷新

--- memory.py
+++ memory.py
@@ -23,6 +23,11 @@ class memory():
             print 'DataBase error'
             return None
         cur = conn.cursor()
+        namecount = cur.execute("select * from song WHERE name = '%s'" % basename)
+        if namecount > 0:
+            print 'the song has been record!'
+            return None
+
         cur.execute("insert into song VALUES(NULL,'%s' );"%basename)
         conn.commit()
         cur.execute("select * from song WHERE  name='%s'"%basename)
@@ -33,16 +38,19 @@ class memory():
         for i in v.hashlist:
             count = cur.execute("select * from fp WHERE fingerprint.fp.fingerprint = '%d'"%i[0])
             if count > 0:
+                cur.execute("select * from fp WHERE fingerprint.fp.fingerprint = '%d'" % i[0])
                 temp = cur.fetchone()
-                print temp[0]
                 id_temp = temp[1] +tuple_result[0].__str__()+ ' ' + i[1].__str__() + ';'
+                print 'id_temp', id_temp
                 cur.execute("UPDATE fingerprint.fp set id = '%s' WHERE fingerprint = '%s'"%(id_temp,temp[0]))
             else:
-                print i[0]
-                print tuple_result[0].__str__()+' '+ i[1].__str__() + ';'
+                # print i[0]
+                # print tuple_result[0].__str__()+' '+ i[1].__str__() + ';'
                 cur.execute("insert into fp VALUES(%d,'%s')"%(i[0],tuple_result[0].__str__()+' '+ i[1].__str__() + ';'))
         conn.commit()
         cur.close()
         conn.close()
-sss = memory('localhost',3306,'root','','fingerprint')
-sss.AddNewSong('C:\data\music\\audio\\audio\\ (1).wav')
+
+    def
+sss = memory('localhost', 3306,'root','','fingerprint')
+sss.AddNewSong('C:\data\music\\audio\\audio\\ (2).wav')
