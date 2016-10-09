991f5f8730cde56e29e24ca958d50a7dc288ab15
chuxiuhong <nanwugongzi@vip.qq.com>
2016-10-09 23:50:30 +0800

    add temp.py for steam game mountain

--- memory.py
+++ memory.py
@@ -5,20 +5,23 @@ import os
 import MySQLdb
 import pyaudio
 
+
 class memory():
-    def __init__(self,host,port,user,passwd,db):
+    def __init__(self, host, port, user, passwd, db):
         self.host = host
         self.port = port
         self.user = user
         self.passwd = passwd
         self.db = db
+
     def AddNewSong(self, path):
         if type(path) != str:
             print 'path need string'
             return None
         basename = os.path.basename(path)
         try:
-            conn = MySQLdb.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd, db=self.db,charset='utf8')
+            conn = MySQLdb.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd, db=self.db,
+                                   charset='utf8')
         except:
             print 'DataBase error'
             return None
@@ -28,29 +31,30 @@ class memory():
             print 'the song has been record!'
             return None
 
-        cur.execute("insert into song VALUES(NULL,'%s' );"%basename)
+        cur.execute("insert into song VALUES(NULL,'%s' );" % basename)
         conn.commit()
-        cur.execute("select * from song WHERE  name='%s'"%basename)
-        tuple_result = cur.fetchone()#存储歌曲的id和name
+        cur.execute("select * from song WHERE  name='%s'" % basename)
+        tuple_result = cur.fetchone()  # 存储歌曲的id和name
         v = pyaudio.voice()
         v.loaddata(path)
         v.fft()
         for i in v.hashlist:
-            count = cur.execute("select * from fp WHERE fingerprint.fp.fingerprint = '%d'"%i[0])
+            count = cur.execute("select * from fp WHERE fingerprint.fp.fingerprint = '%d'" % i[0])
             if count > 0:
                 cur.execute("select * from fp WHERE fingerprint.fp.fingerprint = '%d'" % i[0])
                 temp = cur.fetchone()
-                id_temp = temp[1] +tuple_result[0].__str__()+ ' ' + i[1].__str__() + ';'
+                id_temp = temp[1] + tuple_result[0].__str__() + ' ' + i[1].__str__() + ';'
                 print 'id_temp', id_temp
-                cur.execute("UPDATE fingerprint.fp set id = '%s' WHERE fingerprint = '%s'"%(id_temp,temp[0]))
+                cur.execute("UPDATE fingerprint.fp set id = '%s' WHERE fingerprint = '%s'" % (id_temp, temp[0]))
             else:
                 # print i[0]
                 # print tuple_result[0].__str__()+' '+ i[1].__str__() + ';'
-                cur.execute("insert into fp VALUES(%d,'%s')"%(i[0],tuple_result[0].__str__()+' '+ i[1].__str__() + ';'))
+                cur.execute(
+                    "insert into fp VALUES(%d,'%s')" % (i[0], tuple_result[0].__str__() + ' ' + i[1].__str__() + ';'))
         conn.commit()
         cur.close()
         conn.close()
 
-    def
-sss = memory('localhost', 3306,'root','','fingerprint')
+
+sss = memory('localhost', 3306, 'root', '', 'fingerprint')
 sss.AddNewSong('C:\data\music\\audio\\audio\\ (2).wav')
--- temp.py
+++ temp.py
@@ -1,15 +1,61 @@
 # coding=utf-8
+import os
 import re
-import requests
+import wave
 
-p = re.compile(r'class="fl ei"/><div class="et">')
-text = requests.get('http://library.applinzi.com').text
-# print text
-print p.findall(text)
-'''
-for i in range(1140310503,1140310730):
-    url = 'http://'+i.__str__()+'.applinzi.com'
-    text = requests.get(url).text
-    if len(p.findall(text)) == 0:
-        print i,'found!!!!!!!!!'
-'''
+import numpy as np
+
+
+class voice():
+    def loaddata(self, filepath):
+        '''
+
+        :param filepath: 文件路径，为wav文件
+        :return: 如果无异常则返回True，如果有异常退出并返回False
+        self.wave_data内储存着多通道的音频数据，其中self.wave_data[0]代表第一通道
+        具体有几通道，看self.nchannels
+        '''
+        if type(filepath) != str:
+            print 'the type of filepath must be string'
+            return False
+        p1 = re.compile('\.wav')
+        if p1.findall(filepath) == None:
+            print 'the suffix of file must be .wav'
+            return False
+        try:
+            f = wave.open(filepath, 'rb')
+            params = f.getparams()
+            self.nchannels, self.sampwidth, self.framerate, self.nframes = params[:4]
+            str_data = f.readframes(self.nframes)
+            self.wave_data = np.fromstring(str_data, dtype=np.short)
+            self.wave_data.shape = -1, self.sampwidth
+            self.wave_data = self.wave_data.T
+            f.close()
+            self.name = os.path.basename(filepath)  # 记录下文件名
+            return True
+        except:
+            print 'File Error!'
+
+    def fft(self, frames=50):
+        '''
+        :param frames: frames是指定每秒钟分块数
+        :return:
+        '''
+        try:
+            blocks = []
+            self.high_point = []
+            blocks_size = self.framerate / frames  # block_size为每一块的frame数量
+            blocks_num = self.nframes / blocks_size  # 将音频分块的数量
+            for i in xrange(blocks_num - 1):
+                blocks.append(np.abs(np.fft.fft(self.wave_data[0][i:i + blocks_size])))  #
+            print len(blocks[1])
+        except:
+            print 'data error to fft!'
+            return False
+
+if __name__ == '__main__':
+    p = voice()
+
+    p.loaddata('output2.wav')
+    p.fft()
+    print p.name
