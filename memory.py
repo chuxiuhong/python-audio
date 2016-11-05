474bfe395c848314aba1341072d70ccc490facfb
chuxiuhong <nanwugongzi@vip.qq.com>
2016-11-05 19:36:33 +0800

    完成了指纹重构，数据库写入

--- memory.py
+++ memory.py
@@ -15,7 +15,14 @@ class memory():
         self.passwd = passwd
         self.db = db
 
-    def AddNewSong(self, path):
+    @staticmethod
+    def compare(a, b):
+        if a[1] > b[1]:
+            return True
+        else:
+            return False
+
+    def addsong(self, path):
         if type(path) != str:
             print 'path need string'
             return None
@@ -27,47 +34,44 @@ class memory():
             print 'DataBase error'
             return None
         cur = conn.cursor()
-        namecount = cur.execute("select * from song WHERE name = '%s'" % basename)
+        namecount = cur.execute("select * from fingerprint.musicdata WHERE song_name = '%s'" % basename)
         if namecount > 0:
             print 'the song has been record!'
             return None
-
-        cur.execute("insert into song VALUES(NULL,'%s' );" % basename)
-        conn.commit()
-        cur.execute("select * from song WHERE  name='%s'" % basename)
-        tuple_result = cur.fetchone()  # 存储歌曲的id和name
-        # cur.execute("insert into song_finger VALUES ('%s',NULL)" % tuple_result[0].__str__())
         v = my_audio.voice()
         v.loaddata(path)
         v.fft()
-        for i in v.hashlist:
-            count = cur.execute("select * from fp WHERE fingerprint.fp.fingerprint = '%d'" % i[0])
-            if count > 0:
-                cur.execute("select * from fp WHERE fingerprint.fp.fingerprint = '%d'" % i[0])
-                temp = cur.fetchone()
-                id_temp = temp[1] + tuple_result[0].__str__() + ' ' + i[1].__str__() + ';'  # 用来更新的结果，在数据库中是TEXT类型
-                # print 'id_temp', id_temp
-                cur.execute("UPDATE fingerprint.fp set id = '%s' WHERE fingerprint = '%s'" % (id_temp, temp[0]))
-            else:
-                # print i[0]id
-                # print tuple_result[0].__str__()+' '+ i[1].__str__() + ';'
-                cur.execute(
-                    "insert into fp VALUES(%d,'%s')" % (i[0], tuple_result[0].__str__() + ' ' + i[1].__str__() + ';'))
+        cur.execute("insert into fingerprint.musicdata VALUES('%s','%s')" % (basename, v.high_point.__str__()))
         conn.commit()
         cur.close()
         conn.close()
+
     @staticmethod
-    def compare(a,b):
-        if a[1] > b[1]:
-            return True
-        else:
-            return False
-    def simple_search(self, finger_print):
+    def fp_compare(search_fp, match_fp):
         '''
 
-        :param finger_print: 音频提取出的指纹
-        :return:
+        :param search_fp: 查询指纹
+        :param match_fp: 库中指纹
+        :return:最大相似值 int
         '''
+        if len(search_fp) >= len(match_fp):
+            return 0
+        max_similar = 0
+        search_fp_len = len(search_fp)
+        match_fp_len = len(match_fp)
+        for i in range(match_fp_len - search_fp_len):
+            temp = 0
+            for j in range(search_fp_len):
+                if match_fp[i+j] == search_fp[j]:
+                    temp += 1
+            if temp > max_similar:
+                max_similar = temp
+        return max_similar
+
+    def search(self,path):
+        v = my_audio.voice()
+        v.loaddata(path)
+        v.fft()
         try:
             conn = MySQLdb.connect(host=self.host, port=self.port, user=self.user, passwd=self.passwd, db=self.db,
                                    charset='utf8')
@@ -75,32 +79,14 @@ class memory():
             print 'DataBase error'
             return None
         cur = conn.cursor()
-        # cur.execute("select * from fp WHERE ")
-        song_find_times_dic = {}
-        song_find_times_list = []
-        for i in finger_print:
-            search_str = i[0].__str__()
-            cur.execute("select * from fingerprint.fp WHERE fingerprint = '%s'" % search_str)
-            point_loc = cur.fetchone()[1]
-            temp = point_loc.split(';')[:-1]
-            for j in temp:
-                find_id = j.split(' ')[0]
-                if song_find_times_dic.has_key(find_id):
-                    song_find_times_dic[find_id] += 1
-                else:
-                    song_find_times_dic[find_id] = 1
-        for i in song_find_times_dic.keys():
-            song_find_times_list.append((i,song_find_times_dic[i]))
-        song_find_times_list.sort(cmp=self.compare)
-        print song_find_times_list
-
+        cur.execute("select * from fingerprint.musicdata")
+        result = cur.fetchall()
+        for i in result:
+            memory.fp_compare(v.high_point[100:300],eval(i[1]))
+        cur.close()
+        conn.close()
 
 if __name__ == '__main__':
-    v = my_audio.voice()
-    v.loaddata('s:\\song\\1.wav')
-    v.fft()
-    sss = memory('localhost', 3306, 'root', 'root', 'fingerprint')
-    # sss.AddNewSong('1.wav')
-    for i in range(1,14):
-        sss.AddNewSong('s:\\song\\'+i.__str__()+'.wav')
-    sss.simple_search(v.hashlist)
+    sss = memory('localhost', 3306, 'root', '', 'fingerprint')
+    sss.addsong('1.wav')
+    sss.search('1.wav')
--- my_audio.py
+++ my_audio.py
@@ -28,7 +28,7 @@ class voice():
             self.nchannels, self.sampwidth, self.framerate, self.nframes = params[:4]
             str_data = f.readframes(self.nframes)
             self.wave_data = np.fromstring(str_data, dtype=np.short)
-            self.wave_data.shape = -1, self.sampwidth
+            self.wave_data.shape = -1,self.sampwidth
             self.wave_data = self.wave_data.T
             f.close()
             self.name = os.path.basename(filepath)  # 记录下文件名
@@ -36,32 +36,50 @@ class voice():
         except:
             print 'File Error!'
 
+    @staticmethod
+    def help_cmp(a, b):
+        '''
+
+        :param a: 二元组a
+        :param b: 二元组b
+        :return: a[0]>b[0]布尔型
+        '''
+        if a[0]>b[0]:
+            return True
+        else:
+            return False
+
     def fft(self, frames=50):
         '''
         :param frames: frames是指定每秒钟分块数
         :return:
         '''
-        try:
-            blocks = []
-            self.high_point = []
-            blocks_size = self.framerate / frames  # block_size为每一块的frame数量
-            blocks_num = self.nframes / blocks_size  # 将音频分块的数量
-            for i in xrange(blocks_num - 1):
-                blocks.append(np.abs(np.fft.fft(self.wave_data[0][i:i + blocks_size])))  #
-            for i in xrange(len(blocks)):
-                self.high_point.append(
-                    (np.argmax(blocks[i][40:100]), np.argmax(blocks[i][100:160]), np.argmax((blocks[i][160:220])),
-                     np.argmax(blocks[i][220:280]), np.argmax(blocks[i][280:340])))
-                # high_point存储着fft之后在每个频段的峰值点，存储对象为元组
-            self.timelist = []
-            for i in xrange(len(blocks)):
-                self.timelist.append(blocks_size * i * 1.0 / self.framerate)  # 对每个指纹存储在音频中的时间位置
-            self.hashlist = []
-            for i in xrange(len(blocks)):
-                self.hashlist.append((hash(self.high_point[i]), self.timelist[i]))  # hashlist存储着hash值和时间点的元组
+        blocks = []
+        self.high_point = []
+        blocks_size = self.framerate / frames  # block_size为每一块的frame数量
+        blocks_num = self.nframes / blocks_size  # 将音频分块的数量
+        for i in xrange(blocks_num - 1):
+            blocks.append(np.abs(np.fft.fft(self.wave_data[0][i:i + blocks_size])))  #
+        for i in xrange(len(blocks)):
+            self.high_point.append(
+                (np.argmax(blocks[i][40:100]), np.argmax(blocks[i][100:160])+100, np.argmax((blocks[i][160:220]))+160,
+                 np.argmax(blocks[i][220:280])+220, np.argmax(blocks[i][280:340])+280))
+            temp_list = []
+            for j in range(len(self.high_point[-1])):
+                temp_list.append((blocks[i][self.high_point[-1][j]], j))
+            temp_list = sorted(temp_list)
+            for j in range(len(temp_list)):
+                temp_list[j] = temp_list[j][1]
+            self.high_point[-1] = temp_list
+            # high_point存储着fft之后在每个频段的峰值点，存储对象为元组
+        self.timelist = []
+        for i in xrange(len(blocks)):
+            self.timelist.append(blocks_size * i * 1.0 / self.framerate)  # 对每个指纹存储在音频中的时间位置
+        '''
         except:
             print 'data error to fft!'
             return False
+            '''
 
 
 if __name__ == '__main__':
@@ -69,6 +87,4 @@ if __name__ == '__main__':
 
     p.loaddata('1.wav')
     p.fft()
-    for i in p.hashlist:
-        print i
     print p.name
